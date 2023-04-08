72969ab210ffe57fd92d140183184416fa31afd4
Tushar-2003 <Tushargupta3002@gmail.com>
2023-04-08 17:22:22 +0530

    db created

--- app.py
+++ app.py
@@ -3,6 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    profile_pic = db.Column(db.LargeBinary, nullable=False)
+    twitter = db.Column(db.String(120), nullable=True)
+    linkedin = db.Column(db.String(120), nullable=True)
+    instagram = db.Column(db.String(120), nullable=True)
+    
+    def __repr__(self):
+        return '<User %r>' % self.name
 
 
 @app.route("/")
