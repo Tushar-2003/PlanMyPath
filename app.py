bfb2ff5709cb01994824d84023d791c82f2d1b9d
Tushar-2003 <Tushargupta3002@gmail.com>
2023-04-08 15:31:03 +0530

    more

--- app.py
+++ app.py
@@ -4,9 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['POST','GET'])
 def home():
+    
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    
+    return render_template('login.html')
+
+
+
 
 if __name__ == "__main__" :
     app.run(debug=True)
