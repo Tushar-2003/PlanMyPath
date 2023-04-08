0d8e40861585c2bde534dc2ffcee3e8431f4e15d
Tushar-2003 <Tushargupta3002@gmail.com>
2023-04-08 14:54:13 +0530

    landing page added

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hi"
+    return render_template('index.html')
+
+if __name__ == "__main__" :
+    app.run(debug=True)
