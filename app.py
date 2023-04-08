8ed4be7eae5d531c6a4bfc0d54059303ddc3767a
Tushar-2003 <Tushargupta3002@gmail.com>
2023-04-08 17:55:58 +0530

    singup fixes

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 db = SQLAlchemy(app)
 
 
-class users(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -28,6 +30,31 @@ def index():
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+        profile_pic = request.files['profile_pic']
+        twitter = request.form['twitter']
+        linkedin = request.form['linkedin']
+        instagram = request.form['instagram']
+
+        # Save the uploaded profile pic to the uploads folder
+        filename = secure_filename(profile_pic.filename)
+        profile_pic_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        profile_pic.save(profile_pic_path)
+
+        # Create a new User object and add it to the database
+        user = User(name=name, email=email, password=password, profile_pic=open(profile_pic_path, 'rb').read(), twitter=twitter, linkedin=linkedin, instagram=instagram)
+        db.session.add(user)
+        db.session.commit()
+        
+        return 'Thanks for signing up!'
+
+
 
 
 
